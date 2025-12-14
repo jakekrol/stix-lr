ce333c99b4d1e3a6c56a0a72be6606ef507f34f1
jakekrol <jdkrol351@gmail.com>
2025-12-14 11:43:52 -0700

    stix lr cosmic

---
+++ data/2025_12-cosmic_stix_lr/README.md
@@ -0,0 +1,3 @@
+- prior stix lr results for cosmic
+
+https://raw.githubusercontent.com/ryanlayerlab/lr_stix_analysis/refs/heads/main/data/lr_cosmic_pop_freq_t_5.bed
---
+++ data/2025_12-cosmic_stix_lr/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+url_5="https://raw.githubusercontent.com/ryanlayerlab/lr_stix_analysis/refs/heads/main/data/lr_cosmic_pop_freq_t_5.bed"
+url_1="https://raw.githubusercontent.com/ryanlayerlab/lr_stix_analysis/refs/heads/main/data/lr_cosmic_pop_freq_t_1.bed"
+
+wget "$url_5"
+wget "$url_1"
