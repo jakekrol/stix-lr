68ffb6d9c0bba3e2a95846dc9916c42f26beb911
jakekrol <jdkrol351@gmail.com>
2026-01-19 20:33:41 -0700

    cln cosmic svaf dir

--- results/2026_01-cosmic-svafotate/run.sh
+++ results/2026_01-cosmic-svafotate/run.sh
@@ -2,7 +2,7 @@
 # run conda activate svafotate-env first
 ../../src/svafotate-hg002_cmrg.py \
   --input ../2026_01-cosmic-tsv-to-vcf/cosmic.v103.grch38.vcf \
-  --outdir ../2026_01-cosmic-svafotate \
+  --outdir $(pwd) \
   --bed "$SVAFOTATE_BED" \
   --env_name svafotate-env \
   --timefile ../2026_01-cosmic-svafotate/svafotate_cosmic.times \
