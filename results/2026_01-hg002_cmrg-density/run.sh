e9b1ed401a790e93c496db77d4721ff87e2e2931
jakekrol <jdkrol351@gmail.com>
2026-08-03 19:49:04 -0600

    hg002 cmrg density update needlr results

--- results/2026_01-hg002_cmrg-density/run.sh
+++ results/2026_01-hg002_cmrg-density/run.sh
@@ -14,13 +14,13 @@ hg002_stix_min_read5=$dir_hg002_stix/hg002_cmrg.stix_lr.min_read_5.popfreq.tsv
 ### svafotate
 dir_hg002_svafotate=../2025_12-hg002_cmrg-svafotate
 hg002_svafotate_ov05=$dir_hg002_svafotate/svafotate-hg002-cmrg-overlap_0.5_maxpopfreq.txt
-hg002_svafotate_ov06=$dir_hg002_svafotate/svafotate-hg002-cmrg-overlap_0.6_maxpopfreq.txt
 hg002_svafotate_ov07=$dir_hg002_svafotate/svafotate-hg002-cmrg-overlap_0.7_maxpopfreq.txt
-hg002_svafotate_ov08=$dir_hg002_svafotate/svafotate-hg002-cmrg-overlap_0.8_maxpopfreq.txt
 hg002_svafotate_ov09=$dir_hg002_svafotate/svafotate-hg002-cmrg-overlap_0.9_maxpopfreq.txt
 ### needlr
-dir_hg002_needlr=../2026_01-hg002_cmrg-needlr
-hg002_needlr=$dir_hg002_needlr/hg002_needlr_popfreqs.txt
+dir_hg002_needlr=../2026_07-hg002_cmrg-needlr
+hg002_needlr_ov05=$dir_hg002_needlr/needLR_ov0.5/needlr_pop_freq.tsv
+hg002_needlr_ov07=$dir_hg002_needlr/needLR_ov0.7/needlr_pop_freq.tsv
+hg002_needlr_ov09=$dir_hg002_needlr/needLR_ov0.9/needlr_pop_freq.tsv
 
 # transform stix sample counts to population frequencies
 tail -n +2 $hg002_stix_min_read1 | \
@@ -31,17 +31,24 @@ tail -n +2 $hg002_stix_min_read5 | \
     sort -nr > hg002-stix_lr-min_read5-popfreqs.txt
 
 # extract popfreq column for svafotate files
-for x in 05 06 07 08 09; do
+for x in 05 07 09; do
     infile="hg002_svafotate_ov${x}"
     outfile="hg002-svafotate-ov${x}-popfreqs.txt"
+    tail -n +2 ${!infile} | cut -f2 | sort -gr > $outfile
+done
+
+# extract popfreq column for needlr files
+for x in 05 07 09; do
+    infile="hg002_needlr_ov${x}"
+    outfile="hg002-needlr-ov${x}-popfreqs.txt"
     tail -n +2 ${!infile} | cut -f2 | sort -gr > $outfile
 done
 
 # plot density distributions
 python $script \
     --figsize 6 5 \
-    --inputs "hg002-stix_lr-min_read1-popfreqs.txt,hg002-stix_lr-min_read5-popfreqs.txt,hg002-svafotate-ov05-popfreqs.txt,hg002-svafotate-ov06-popfreqs.txt,hg002-svafotate-ov07-popfreqs.txt,hg002-svafotate-ov08-popfreqs.txt,hg002-svafotate-ov09-popfreqs.txt,$hg002_needlr" \
-    --names "STIX-LR;MR=1,STIX-LR;MR=5,SVAFotate;OV=0.5,SVAFotate;OV=0.6,SVAFotate;OV=0.7,SVAFotate;OV=0.8,SVAFotate;OV=0.9,NeedLR" \
+    --inputs "hg002-stix_lr-min_read1-popfreqs.txt,hg002-stix_lr-min_read5-popfreqs.txt,hg002-svafotate-ov05-popfreqs.txt,hg002-svafotate-ov07-popfreqs.txt,hg002-svafotate-ov09-popfreqs.txt,hg002-needlr-ov05-popfreqs.txt,hg002-needlr-ov07-popfreqs.txt,hg002-needlr-ov09-popfreqs.txt" \
+    --names "STIX-LR;MR=1,STIX-LR;MR=5,SVAFotate;OV=0.5,SVAFotate;OV=0.7,SVAFotate;OV=0.9,NeedLR;OV=0.5,NeedLR;OV=0.7,NeedLR;OV=0.9" \
     --title "HG002 CMRG SVs" \
     --xlabel "" \
     --ylabel "Population frequency" \
