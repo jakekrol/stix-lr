eaddf9ece395235cd66fbc2f3c13222a64ba60a3
jakekrol <jdkrol351@gmail.com>
2026-08-21 13:27:43 -0600

    results: 1kg stixlr reuse addid rename file

--- results/2026_08-1kg-stixlr-reuse-add_id/run.sh
+++ results/2026_08-1kg-stixlr-reuse-add_id/run.sh
@@ -2,4 +2,4 @@
 
 ./addid_1kg_stixlr_results.py
 
-awk -v OFS='\t' '{print $6, $5}' lr_1kg_pop_freq_t_5.addID.bed > lr_1kg_pop_freq_t_5.addID.tsv
+awk -v OFS='\t' '{print $6, $5}' lr_1kg_pop_freq_t_5.addID.bed > onekg-stix_lr-min_read_5.popfreq.tsv
